306b38b32fa946987f950a906495a215dd91710d
Ctonkin <caleb.tonkin1@gmail.com>
2024-03-01 16:42:40 +1030

    edits

--- Human.cpp
+++ Human.cpp
@@ -5,10 +5,24 @@
 Human::Human(std::string name) : name(name) {}
 
 Move* Human::makeMove() {
-    Move* move;
+    std::string move;
     std::cout << "Enter move: ";
     std::cin >> move;
-    return move;
+    
+    if (move == "Rock"){
+        return new Rock();
+    }
+    else if (move == "Paper"){
+        return new Paper();
+    }
+    else if (move == "Scissors"){
+        return new Scissors();
+    }
+    else{
+        std::cout << "Invalid move. Try again." << std::endl;
+        return nullptr;
+    
+    }
 }
 
 std::string Human::getName(){
