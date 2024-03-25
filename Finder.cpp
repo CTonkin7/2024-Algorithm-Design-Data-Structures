32e7a5c567e607aef8f0ae20b649d404e4d51dc1
Ctonkin <caleb.tonkin1@gmail.com>
2024-03-25 12:53:48 +1030

    finder edits

--- Finder.cpp
+++ Finder.cpp
@@ -12,7 +12,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
     size_t index = 0;
     for (size_t i = 1; i <= s2.size(); ++i) {
         std::string prefix = s2.substr(0, i);
-        size_t found = s1.find(prefix, index); // Start search from the max index found
+        size_t found = s1.find(prefix, index); 
         
         if (found != string::npos) {
             result.push_back(found);
@@ -20,11 +20,11 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
             continue;
         } else {
             result.push_back(-1);
-            continue;  // No need to search further for this prefix
+            break;  
         }
     }
 
-    // If no occurrence is found for any prefix, push -1 into the result vector
+
     if (result.empty()) {
         result.push_back(-1);
     }
