894f7f340c6152de516ef68b8fe7771b85332601
Ctonkin <caleb.tonkin1@gmail.com>
2024-03-30 19:26:19 +1030

    Bubblesort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,13 +5,13 @@ std::vector<int> BubbleSort::sort(std::vector<int> list) {
     bool swapped = true;
     while (swapped) {
         swapped = false;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i++) {
             if (list[i - 1] > list[i]) {
                 std::swap(list[i - 1], list[i]);
                 swapped = true;
             }
         }
-        --n;
+        n--;
     }
     return list;
 }
