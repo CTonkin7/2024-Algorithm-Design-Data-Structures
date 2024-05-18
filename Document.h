28b52b4a3a2df003cd815920bab1490506227868
Ctonkin <caleb.tonkin1@gmail.com>
2024-05-19 00:08:40 +0930

    default const

--- Document.h
+++ Document.h
@@ -14,6 +14,7 @@ class Document {
         int current_borrowed;
         std::unordered_set<int> borrowed_by;
 
+        Document() = default;
         Document(std::string name, int id, int license_limit);
 };
 
