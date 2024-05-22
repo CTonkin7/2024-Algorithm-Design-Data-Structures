d60cd31e3297f1734e94e5a1996391dc2aa268e9
Ctonkin <caleb.tonkin1@gmail.com>
2024-05-22 14:56:02 +0930

    edits

--- DocumentManager.h
+++ DocumentManager.h
@@ -7,7 +7,17 @@
 #include <string>
 
 using namespace std;
+class Document {
+    public:
+        std::string name;
+        int id;
+        int license_limit;
+        int borrowed_count;
 
+        Document() : id(0), license_limit(0), borrowed_count(0) {}
+        Document(std::string name, int id, int license_limit) 
+            : name(name), id(id), license_limit(license_limit), borrowed_count(0) {}
+};
 class DocumentManager {
 private:
     std::unordered_map<int, Document> documents_by_id;
