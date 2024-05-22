616e1ab8f59489be7c799fba8ee01680d8fc7b56
Ctonkin <caleb.tonkin1@gmail.com>
2024-05-22 15:03:43 +0930

    edits

--- DocumentManager.h
+++ DocumentManager.h
@@ -5,7 +5,7 @@
 #include <unordered_set>
 #include <string>
 
-using namespace std;
+
 class Document { // Document.h
     public:
         std::string name;
@@ -14,13 +14,13 @@ class Document { // Document.h
         int borrowed_count;
 
         Document() : id(0), license_limit(0), borrowed_count(0) {}
-        Document(std::string name, int id, int license_limit) 
-            : name(name), id(id), license_limit(license_limit), borrowed_count(0) {}
+        Document(std::string n, int i, int limit) 
+            : name(n), id(i), license_limit(limit), borrowed_count(0) {}
 };
 class DocumentManager {
 private:
     std::unordered_map<int, Document> documents_by_id;
-    std::unordered_map<int, Document> documents_by_name;
+    std::unordered_map<std::string, int> documents_by_name;
     std::unordered_set<int> patrons;
     std::unordered_map<int, std::unordered_set<int>> borrowed_documents;
 
